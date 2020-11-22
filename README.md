bb83a793fe6cf605bf83d4810647edb0721e45a5
David Merrick <david@david-merrick.com>
2020-11-21 17:41:21 -0800

    Got calendar fetching working

---
+++ README.md
@@ -0,0 +1,17 @@
+Ever get annoyed when you subscribe to a calendar, like, say for an on-call schedule,
+and the events are set up to block your entire schedule?
+
+![](/docs/img/screenshot.png)
+
+Yeah, me too. That's why I created this.
+
+It's a web service that takes a calendar url and 
+gives you back the same calendar with the events
+converted to all-day events.
+
+![](/docs/img/screenshot-after.png)
+
+Ah, that's better.
+
+Better living, with Dayify™.
+
--- src/main/kotlin/io/github/davidmerrick/dayify/controllers/DayifyController.kt
+++ src/main/kotlin/io/github/davidmerrick/dayify/controllers/DayifyController.kt
@@ -1,19 +1,37 @@
 package io.github.davidmerrick.dayify.controllers
 
-import io.micronaut.http.MediaType
+import biweekly.Biweekly
+import io.github.davidmerrick.dayify.logic.CalendarConverter
+import io.micronaut.http.HttpResponse
 import io.micronaut.http.annotation.Controller
 import io.micronaut.http.annotation.Get
 import io.micronaut.http.annotation.QueryValue
+import io.micronaut.http.client.HttpClient
+import io.micronaut.http.client.annotation.Client
 import mu.KotlinLogging
 
 private val log = KotlinLogging.logger {}
 
-@Controller("/dayify")
-class DayifyController {
+private const val CAL_MEDIA_TYPE = "text/calendar"
 
-    @Get("/", produces = [MediaType.TEXT_PLAIN])
-    fun dayify(@QueryValue url: String): String? {
+@Controller("/dayify")
+class DayifyController(@Client private val client: HttpClient) {
+
+    @Get("/", produces = [CAL_MEDIA_TYPE])
+    fun dayify(@QueryValue url: String): HttpResponse<String> {
         log.info("Fetching calendar: $url")
-        return "Success"
+        val responseBody = client.toBlocking()
+            .exchange(url, String::class.java)
+            .body()
+
+        val inCalendar = try {
+            Biweekly.parse(responseBody).first()
+        } catch (e: Exception) {
+            log.warn("Failed to parse calendar", e)
+            return HttpResponse.badRequest()
+        }
+
+        val outCalendar = CalendarConverter.convert(inCalendar)
+        return HttpResponse.ok(Biweekly.write(outCalendar).go())
     }
 }
