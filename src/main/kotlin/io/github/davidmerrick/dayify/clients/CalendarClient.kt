51e80b2ab9334194e7d8b16deb6daa4567231dc0
David Merrick <david@david-merrick.com>
2022-09-21 17:56:05 -0700

    Added coroutines for client (#1)
    * Initial foray into coroutines
    
    * Got bridge working between rxJava and coroutines

--- src/main/kotlin/io/github/davidmerrick/dayify/clients/CalendarClient.kt
+++ src/main/kotlin/io/github/davidmerrick/dayify/clients/CalendarClient.kt
@@ -2,28 +2,34 @@ package io.github.davidmerrick.dayify.clients
 
 import biweekly.Biweekly
 import biweekly.ICalendar
-import io.micronaut.http.HttpHeaders
+import io.micronaut.http.HttpHeaders.USER_AGENT
 import io.micronaut.http.HttpRequest
-import io.micronaut.http.client.HttpClient
+import io.micronaut.http.client.RxHttpClient
 import io.micronaut.http.client.annotation.Client
+import kotlinx.coroutines.rx2.await
 import mu.KotlinLogging
 import javax.inject.Singleton
 
 private val log = KotlinLogging.logger {}
 
+private const val USER_AGENT_HEADER = "Dayify/1.0"
+
 @Singleton
-class CalendarClient(
-    @field:Client private val client: HttpClient
-) {
-    fun fetchCalendar(url: String): ICalendar {
+class CalendarClient(@param:Client private val client: RxHttpClient) {
+
+    suspend fun fetchCalendar(url: String): ICalendar {
         log.info("Fetching calendar: $url")
         val request = HttpRequest.GET<String>(url)
-            .header(HttpHeaders.USER_AGENT, "Dayify/1.0")
-        val responseBody = client
-            .toBlocking()
-            .retrieve(request)
+            .header(USER_AGENT, USER_AGENT_HEADER)
+        return client.retrieve(request)
+            .firstOrError()
+            .await()
+            .let { parseCalendar(it) }
+    }
+
+    private fun parseCalendar(body: String): ICalendar {
         return try {
-            Biweekly.parse(responseBody).first()
+            Biweekly.parse(body).first()
         } catch (e: Exception) {
             log.warn("Failed to parse calendar", e)
             throw e
